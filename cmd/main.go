8ac2bbd6360b7841d05b71a1f7d9bd86a6d4e8f8
Oleksandr Redko <oleksandr.red+github@gmail.com>
2022-12-09 16:40:55 +0200

    Change output format about online

--- cmd/main.go
+++ cmd/main.go
@@ -11,16 +11,20 @@ import (
 )
 
 func main() {
-	dataGroupLogin := flag.String("datagroup_login", "", "Login to personal cabinet https://my.datagroup.ua")
-	dataGroupPassword := flag.String("datagroup_password", "", "Password to personal cabinet")
-	if dataGroupLogin == nil || dataGroupPassword == nil {
-		*dataGroupLogin = os.Getenv("DATAGROUP_LOGIN")
-		*dataGroupPassword = os.Getenv("DATAGROUP_PASSWORD")
+	var (
+		dataGroupLogin    string
+		dataGroupPassword string
+	)
+	flag.StringVar(&dataGroupLogin, "datagroup_login", "", "Login to personal cabinet https://my.datagroup.ua")
+	flag.StringVar(&dataGroupPassword, "datagroup_password", "", "Password to personal cabinet")
+	if dataGroupLogin == "" || dataGroupPassword == "" {
+		dataGroupLogin = os.Getenv("DATAGROUP_LOGIN")
+		dataGroupPassword = os.Getenv("DATAGROUP_PASSWORD")
 	}
 
 	provider := datagroup.New(datagroup.Config{
-		Login:    *dataGroupLogin,
-		Password: *dataGroupPassword,
+		Login:    dataGroupLogin,
+		Password: dataGroupPassword,
 	})
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
@@ -31,5 +35,9 @@ func main() {
 		log.Fatalf("Failed to retrive status: %v.", err)
 	}
 
-	log.Printf("Is online DataGroup: %t", isOnline)
+	if isOnline {
+		log.Printf("DataGroup is online\n")
+	} else {
+		log.Printf("DataGroup is offline\n")
+	}
 }
